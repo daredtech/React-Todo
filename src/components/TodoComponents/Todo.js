200043126bd0cf1782595b97c3769b769a8db71d
Red <vhanchar@ucsc.edu>
2019-08-05 12:48:58 -0700

    added the skeleton for todos

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { ReactComponent } from '*.svg';
+
+ class Todo extends React.Component {
+
+    constructor () {
+        super();
+
+    }
+
+    render () {
+        return (
+            <>
+            Todo
+            </>
+        )
+    }
+ }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { ReactComponent } from '*.svg';
+
+ class TodoForm extends React.Component {
+
+    constructor () {
+        super();
+
+    }
+
+    render () {
+        return (
+            <>
+            <p>TodoForm</p>
+            </>
+        )
+    }
+ }
